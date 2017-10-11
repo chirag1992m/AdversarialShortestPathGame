0af125eb17aa3c4587a314486e9a6854c771e119
Chirag Maheshwari <chirag1992m.cc@gmail.com>
2017-10-10 21:14:32 -0400

    Fixing the port type to integer

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ from core.game_looper import GameLooper
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run the Adversarial Game Server.")
-    parser.add_argument('--port', '--p', default=8080, help='Port to run the server on')
+    parser.add_argument('--port', '--p', type=int, default=8080, help='Port to run the server on')
     parser.add_argument('--game-file', default='sample/advshort.txt',
                         help='The game layout file to be loaded.')
 
